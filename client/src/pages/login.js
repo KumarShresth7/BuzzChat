86c186955ec157f17bb6a17719390313aa0c119a
Kumar Shresth <kumarshresth2004@gmail.com>
2024-06-05 20:38:27 +0530

    login and register

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import '../pages/styles/login.css';
 
 const login = () => {
   return (
-    <div>login</div>
-  )
+    <div>
+      <div class="container">
+        <div class="card">
+          <h2>Login</h2>
+          <form>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Username"
+              required
+            />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              required
+            />
+            <a href='/register'>New User?</a>
+            <br/>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default login
--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -2,7 +2,39 @@ import React from 'react'
 
 const register = () => {
   return (
-    <div>register</div>
+    <div>
+      <div class="container">
+        <div class="card">
+          <h2>Register</h2>
+          <form>
+            <input
+            type="text"
+            id="email"
+            name="email"
+            placeholder="Enter your email ID"
+            required
+            />
+            <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Username"
+              required
+            />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              required
+            />
+            <a href='/'>Already a Member?</a>
+            <br/>
+            <button type="submit">Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
