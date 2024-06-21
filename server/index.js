71525ed1f48ba1eb57d62e058a8d84b9d3b2f196
Kumar Shresth <kumarshresth2004@gmail.com>
2024-06-21 18:59:00 +0530

    modfiy

--- server/index.js
+++ server/index.js
@@ -35,6 +35,12 @@ app.use(express.json());
 // Handle CORS preflight requests
 app.options('*', cors());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://buzz-chat-frontend-six.vercel.app');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
