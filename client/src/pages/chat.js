424ef7ae4a89ccb6cc691bde366043c0e04560ac
Kumar Shresth <kumarshresth2004@gmail.com>
2024-06-21 18:21:05 +0530

    #

--- client/src/pages/chat.js
+++ client/src/pages/chat.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../context/userContext';
 
-const socket = io('http://localhost:5000');
+const socket = io('https://buzz-chat-api.vercel.app');
 
 
 const Chat = () => {
