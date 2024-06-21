a7d3c308dc5ac3353eed3db23e75061dbfc53f19
Kumar Shresth <kumarshresth2004@gmail.com>
2024-06-21 19:20:40 +0530

    #

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const io = socketIo(server, {
     origin: "https://buzz-chat-frontend-six.vercel.app",
     handlePreflightRequest: (req, res) => {
       res.writeHead(200, {
-        "Access-Control-Allow-Origin": "https://example.com",
+        "Access-Control-Allow-Origin": "https://buzz-chat-frontend-six.vercel.app",
         "Access-Control-Allow-Methods": "GET,POST",
         "Access-Control-Allow-Headers": "my-custom-header",
         "Access-Control-Allow-Credentials": true
