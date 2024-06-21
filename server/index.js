765091814d378f8dba3f9f517188602736b6c5db
Kumar Shresth <kumarshresth2004@gmail.com>
2024-06-21 18:11:47 +0530

    #

--- server/index.js
+++ server/index.js
@@ -44,3 +44,4 @@ app.use('/api/messages', messageRoutes);
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.options('*', cors());
